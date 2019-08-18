666c5bad9aeda9e86134c72ef1c1eeb37ef19fdc
hatemeby <50153429+hatemeby@users.noreply.github.com>
2019-08-18 21:10:48 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,35 +4,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Numbers numbers = new Numbers();
         Main main = new Main();
+        Result res = new Result();
         Operation operation = new Operation();
         int numberOne = numbers.getInt1();
         int numberTwo = numbers.getInt2();
         String operator = operation.getOperator();
-        int result = main.getResult(numberOne, numberTwo, operator);
+        int result = res.getResult(numberOne, numberTwo, operator);
         System.out.println("Шурик лох " + result + " раз");
     }
 
-    public int getResult(int numberOne, int numberTwo, String operator) throws IOException {
-        Operation operation = new Operation();
-        int res;
-        switch (operator) {
-            case "+":
-                res = numberOne + numberTwo;
-                break;
-            case "-":
-                res = numberOne - numberTwo;
-                break;
-            case "/":
-                res = numberOne/numberTwo;
-                break;
-            case "*":
-                res = numberOne*numberTwo;
-                break;
-            default:
-                System.out.println("Говно коня, идите нахуй");
-                res = getResult(numberOne, numberTwo, operation.getOperator());
-        }
-        return res;
 
-    }
 }
